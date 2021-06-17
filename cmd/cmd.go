f02b904cc7c474e0b2bcb2a8b9513158ca58b5f5
cserli <1528884535@qq.com>
2021-06-17 18:59:29 +0800

    redis sync lock

---
+++ cmd/cmd.go
@@ -0,0 +1,7 @@
+package cmd
+
+// 使用go语言发命令到ffmpeg
+func SendCmd(data *interface{})  {
+
+	return
+}
---
+++ commonlib/init.go
@@ -0,0 +1,7 @@
+package commonlib
+
+// 后期拓展使用，主要是封装ffmpeg库使用，拓展到后面cgo使用
+
+func init()  {
+	
+}
